e2ce4ca110cb30fd31041f1be2effcaa8e314b9e
kelly_maass <kmaass77627@gmail.com>
2025-10-29 13:17:39 -0500

    done

--- myrecipebox/src/index.js
+++ myrecipebox/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';  // ✅ add this import
+import { BrowserRouter } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
